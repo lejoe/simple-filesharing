03acb3b7950456f648f86362d32ad96f645c84c6
lejoe <ciao@lejoe.com>
2024-04-17 11:31:07 +0200

    Adds tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase'],
+  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
   runtimeConfig: {
     s3Url: '',
     s3Bucket: '',
